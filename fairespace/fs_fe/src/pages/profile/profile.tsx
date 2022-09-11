344c2e77f84d96177028c55493cc486fcbd70c29
RossEpsteinKY <31447738+RossEpsteinKY@users.noreply.github.com>
2022-09-11 13:44:17 -0400

    added placeholder profiles

--- fairespace/fs_fe/src/pages/profile/profile.tsx
+++ fairespace/fs_fe/src/pages/profile/profile.tsx
@@ -20,10 +20,65 @@
 
 export default function Profile() {
 
+// friends placeholder
+const friends = [
+  {
+    name: 'Friend Placeholder Name',
+    role: 'Senior Designer',
+    imageUrl:
+      'https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    twitterUrl: '#',
+    linkedinUrl: '#',
+  },  {
+    name: 'Friend Placeholder Name',
+    role: 'Senior Designer',
+    imageUrl:
+      'https://randomuser.me/api/portraits/women/8.jpg',
+    twitterUrl: '#',
+    linkedinUrl: '#',
+  },  {
+    name: 'Friend Placeholder Name',
+    role: 'Senior Designer',
+    imageUrl:
+      'https://randomuser.me/api/portraits/men/2.jpg',
+    twitterUrl: '#',
+    linkedinUrl: '#',
+  },  {
+    name: 'Friend Placeholder Name',
+    role: 'Senior Designer',
+    imageUrl:
+      'https://randomuser.me/api/portraits/men/22.jpg',
+    twitterUrl: '#',
+    linkedinUrl: '#',
+  },  {
+    name: 'Friend Placeholder Name',
+    role: 'Senior Designer',
+    imageUrl:
+      'https://randomuser.me/api/portraits/women/12.jpg',
+    twitterUrl: '#',
+    linkedinUrl: '#',
+  },  {
+    name: 'Friend Placeholder Name',
+    role: 'Senior Designer',
+    imageUrl:
+      'https://randomuser.me/api/portraits/women/90.jpg',
+    twitterUrl: '#',
+    linkedinUrl: '#',
+  },  {
+    name: 'Friend Placeholder Name',
+    role: 'Senior Designer',
+    imageUrl:
+      'https://randomuser.me/api/portraits/women/99.jpg',
+    twitterUrl: '#',
+    linkedinUrl: '#',
+  },
+  // More people...
+]
+// profile placeholder data
 const profile =
   {
     name: 'Emma Dorsey',
-    role: 'Senior Front-end Developer',
+    role: 'Booth Worker',
     imageUrl:
       'https://images.unsplash.com/photo-1505840717430-882ce147ef2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
     bio: 'Ultricies massa malesuada viverra cras lobortis. Tempor orci hac ligula dapibus mauris sit ut eu. Eget turpis urna maecenas cras. Nisl dictum.',
@@ -53,28 +108,7 @@ const profile =
                       <p className="text-gray-500">{profile.bio}</p>
                     </div>
 
-                    <ul role="list" className="flex space-x-5">
-                      <li>
-                        <a href={profile.twitterUrl} className="text-gray-400 hover:text-gray-500">
-                          <span className="sr-only">Twitter</span>
-                          <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
-                          </svg>
-                        </a>
-                      </li>
-                      <li>
-                        <a href={profile.linkedinUrl} className="text-gray-400 hover:text-gray-500">
-                          <span className="sr-only">LinkedIn</span>
-                          <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                            <path
-                              fillRule="evenodd"
-                              d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
-                              clipRule="evenodd"
-                            />
-                          </svg>
-                        </a>
-                      </li>
-                    </ul>
+
                   </div>
                 </li>
 
@@ -87,6 +121,23 @@ const profile =
                                     Nulla quam felis, enim faucibus proin velit, ornare id pretium. Augue ultrices sed arcu condimentum
                                     vestibulum suspendisse. Volutpat eu faucibus vivamus eget bibendum cras.
                                   </p>
+                                  <h2 className="text-3xl font-bold tracking-tight sm:text-4xl">Friends</h2>
+                                  <ul role="list" className="space-y-1 sm:grid sm:grid-cols-1 sm:gap-4  lg:grid-cols-4 ">
+                                              {friends.map((friend) => (
+                                                <li key={friend.name} className=" rounded-full py-4 bg-gray-800  text-center px-4 xl:text-center">
+                                                  <div className="xl:mx-auto">
+                                                    <img className=" mx-auto xl:object-center rounded-full xl:h-20 xl:w-20" src={friend.imageUrl} alt={friend.name + " Profile"} />
+                                                    <div className="space-y-2 xl:flex xl:items-center xl:justify-between">
+                                                      <div className="space-y-1 text-lg  font-medium leading-4">
+                                                        <h3 className="text-white ">{friend.name}</h3>
+                                                      </div>
+
+
+                                                    </div>
+                                                  </div>
+                                                </li>
+                                              ))}
+                                            </ul>
                         </div>
         </div>
 
