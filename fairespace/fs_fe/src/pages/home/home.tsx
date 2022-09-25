0e5fa10fe5d6f20d8659adc939ff22e717d9c48c
RossEpsteinKY <31447738+RossEpsteinKY@users.noreply.github.com>
2022-09-25 15:22:57 -0400

    changed landing page

--- fairespace/fs_fe/src/pages/home/home.tsx
+++ fairespace/fs_fe/src/pages/home/home.tsx
@@ -8,24 +8,7 @@ import {
   ShieldCheckIcon,
 } from '@heroicons/react/24/outline'
 
-const features = [
 
-  {
-    name: 'Advanced Security',
-    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
-    icon: ShieldCheckIcon,
-  },
-  {
-    name: 'Powerful API',
-    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
-    icon: CogIcon,
-  },
-  {
-    name: 'Database Backups',
-    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
-    icon: ServerIcon,
-  },
-]
 
 export default function Home() {
   return (
@@ -35,26 +18,12 @@ export default function Home() {
           RenfaireSpace
         </p>
         <p className="mx-auto mt-5 max-w-prose text-xl text-gray-500">
-          Phasellus lorem quam molestie id quisque diam aenean nulla in. Accumsan in quis quis nunc, ullamcorper
-          malesuada. Eleifend condimentum id viverra nulla.
+          <h3 className="text-2xl">A place for all participants, patrons, and people
+          to share in the magic of the <b>Renaissance Faire.</b></h3>
         </p>
         <div className="mt-12">
           <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-            {features.map((feature) => (
-              <div key={feature.name} className="pt-6">
-                <div className="flow-root rounded-lg bg-gray-50 px-6 pb-8">
-                  <div className="-mt-6">
-                    <div>
-                      <span className="inline-flex items-center justify-center rounded-md bg-indigo-500 p-3 shadow-lg">
-                        <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
-                      </span>
-                    </div>
-                    <h3 className="mt-8 text-lg font-medium tracking-tight text-gray-900">{feature.name}</h3>
-                    <p className="mt-5 text-base text-gray-500">{feature.description}</p>
-                  </div>
-                </div>
-              </div>
-            ))}
+
           </div>
         </div>
       </div>
