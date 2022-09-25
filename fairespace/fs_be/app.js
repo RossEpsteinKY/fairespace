8af2dad8a02d804162872922ac86dd9dc30c724f
RossEpsteinKY <31447738+RossEpsteinKY@users.noreply.github.com>
2022-09-25 13:02:06 -0400

    removed commented out code

--- fairespace/fs_be/app.js
+++ fairespace/fs_be/app.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const {request, response} = require("express");
 const {param} = require("express/lib/router");
 const bodyParser = require('body-parser');
-// const session = require('express-session');
 const session = require('cookie-session');
 const authController = require('./src/controllers/auth/auth.controller');
 const userController = require('./src/controllers/user/user.controller');
@@ -11,23 +10,12 @@ const photosController = require('./src/controllers/photo/photo.controller');
 const accountInfoController = require('./src/controllers/account/account.controller');
 const friendsController = require('./src/controllers/friends/friends.controller');
 const groupController = require('./src/controllers/group/group.controller');
+
 const app = express();
 require('dotenv').config();
 const {
     Model, Sequelize
 } = require('sequelize');
-
-// const quizzesController = require('./src/controllers/quizzes');
-// const questionController = require('./src/controllers/questions');
-// const choicesController = require('./src/controllers/choices');
-// const authController = require('./src/controllers/auth');
-// const limiter = require('express-rate-limit')
-// const { isAuthenticated } = require("./src/middleware/auth");
-// const helmet = require('helmet');
-// const hpp = require('hpp');
-// const csurf = req uire('csurf');
-
-
 
 app.use(
     session({
@@ -38,20 +26,9 @@ app.use(
     })
 );
 
-// var sequelize = new Sequelize(global.config.dbConfig.name, global.config.dbConfig.user, global.config.dbConfig.password, {
-//     host: global.config.dbConfig.host,
-//     port: global.config.dbConfig.port,
-//     pool: false
-// });
-
 app.use(bodyParser.urlencoded({ extended: false }))
 app.set('views',__dirname + '/src/views');
 app.set('view engine','twig');
-
-// app.get('/',(req,res) =>{
-//     // res.send("HOMEPAGE");
-//     res.render('home');
-// })
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -59,19 +36,12 @@ app.use(function(req, res, next) {
     next();
 });
 
-// app.use('/quizzes', authController);
-// app.use('/questions', questionController);
-// app.use('/choices', choicesController);
 app.use('/auth', authController);
 app.use('/user', userController);
 app.use('/photos', photosController);
 app.use('/group', groupController);
 app.use('/account', accountInfoController);
 app.use('/friends', friendsController);
-// app.get('/', isAuthenticated, async  (req,res) => {
-//     console.log('TOKEN', req.session.access_token);
-//     res.render('home');
-// });
 
 app.listen(5000);
 console.log('Listening on port 5000!');
